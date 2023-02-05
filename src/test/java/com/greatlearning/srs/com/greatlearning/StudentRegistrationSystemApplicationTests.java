3470c3162c9a987f78f729e01191fcb03de5d00c
Geethu George <109959204+GeethuGeorge@users.noreply.github.com>
2023-02-05 10:35:03 +0100

    Add files via upload

---
+++ src/test/java/com/greatlearning/srs/com/greatlearning/StudentRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.srs.com.greatlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
